27390ef6c81482cece921ea3df59b389d8c5ad1f
Philipp Graf <PhilippGraf@web.de>
2013-04-10 19:56:19 +0200

    middle of refactor

--- public_html/js/elly.js
+++ public_html/js/elly.js
@@ -1,43 +1,8 @@
 var ELLY = ELLY || {};
 
-/*
- var ruleset = { "R1" : function() {c("R2").call("R1"); },
- "R2" : function() {call("R1"); }};
- */
-
-/*
- var sys = new ELLY.System();
- sys.R1 = function() { R2().R1(); }
- sys.rule("R1", "R2().R1();");
- sys.rule("R1", function() { R2().R1(); });
- sys.rule("R1", "R2 R1");
- 
- 
- var Test = ELLY.System.extend() {
- init: function() {
- this.super();
- }
- 
- build: function() {
- R1 = function() { R2().R1(); };
- eval("R1 = function() { R2().R1(); }");
- }
- }
- 
- sys.R2 = function() { move(10).scale(0.9).R2() }
- */
-
-// new ELLY.Rule("R1", function() {});
-ELLY.State = function(o) {
-    if (o === undefined) {
-        this.position = new THREE.Vector3();
-        this.direction = new THREE.Vector3(1, 0, 0);
-        this.scale = 1.0;
-    } else {
-        this.position = o.position.clone();
-        this.direction = o.direction.clone();
-        this.scale = o.scale;
-    }
+ELLY.State = function() {
+    this.objectProto = new THREE.Object3D();
+    
 };
 
 ELLY.State.prototype = {
@@ -56,27 +21,56 @@ ELLY.System = function(scene, maxDepth) {
     this.depth = 0;
 
     this.state = new ELLY.State();
+    this.scene.add(this.state.objectProto);
 };
+
+
+
 
 ELLY.System.prototype = {
     constuctor : ELLY.System,
-            
+    /*
+     * Define a new rule "name" with given "code"
+     */  
+     
+    
     rule : function(name, code) {
-        this[name] = function(isRoot) {
+        this[name] = function(transforms, isRoot) {
             if (isRoot === true) {
                 saveState = this.state;
-                this.state = new ELLY.State(this.state);
-                new Function(code).call(this);
+                this.state = new ELLY.State();
+                saveState.objectProto.add(this.state.objectProto);
+                
+                this.evalTransforms(transforms);
+                if (code instanceof String) {
+                    new Function(code).call(this);
+                } else {
+                    code.call(this);
+                }
                 this.state = saveState;
             } else if (this.depth < this.maxDepth) {
-                this.backlogBuild.push([name, this.state]);
+                this.backlogBuild.push([name, transforms, this.state]);
             }
             return this;            // method chain
         };
     },
     
-    evalRule: function(name) {
-        this.backlog.push([name, this.state]);
+    evalTransforms : function(transforms) {
+        for(t in transforms) {
+            if (t in this) {
+                this[t].call(this, transforms[t]);
+            } else {
+                console.warn("Skipping unknown transform \"" + t +"\".")
+            }
+        }
+    },
+            
+    /*
+     * Start evaluation with rule "name"
+     */
+    trigger : function(name) {
+        this.backlog.push([name, {}, this.state]);
+        this.scene.add(this.state.objectProto);
 
         this.depth = 0;
         while (this.backlog.length > 0) {
@@ -84,8 +78,8 @@ ELLY.System.prototype = {
             while (this.backlog.length > 0) {
                 console.debug("[RULE] " + this.backlog[0] + ":" + this.depth);
                 var entry = this.backlog.shift();
-                this.state = entry[1];
-                this[entry[0]].call(this, true);
+                this.state = entry[2];
+                this[entry[0]].call(this, entry[1], true);
             }
             this.depth++;
             this.backlog = this.backlogBuild;
@@ -93,32 +87,45 @@ ELLY.System.prototype = {
         }
     },
     
-    move: function(amount) {
-        delta = this.state.direction.clone().multiplyScalar(amount);
-        this.state.position.add(delta);
+    /*
+     * Move forward (scale sensitive)
+     */
+    move : function(amount) {
+        this.state.objectProto.position.z += amount;
         return this;
     },
     
-    scale: function(amount) {
-        this.state.scale *= amount;
-        this.state.direction.multiplyScalar(amount);
+    // turn roll yaw
+       
+    
+    /*
+     * Change scale by factor amount
+     */
+    scale : function(amount) {
+        this.state.objectProto.scale.multiplyScalar(amount);
         return this;
     },
    
-    cube: function(amount) {
+    /*
+     * Draw cube
+     */
+    cube : rule(function() {
         var cubeGeometry = new THREE.CubeGeometry(1, 1, 1);
         var cubeMaterial = new THREE.MeshPhongMaterial({color: 0xcccccc});
 
         var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-        cube.position.copy(this.state.position);
-        cube.scale.set(this.state.scale, this.state.scale, this.state.scale);
-        this.scene.add(cube);
+        this.state.objectProto.clone(cube);
+        this.state.objectProto.parent.add(cube);
 
-        return this;
-        // TODO 
-    }
+//        return this;
+    })
 };
 
-// Shortcuts
+ELLY.System.prototype.cube = //TODO
+
+
+/*
+ * Shortcuts
+ */
 ELLY.System.prototype.m = ELLY.System.prototype.move;
 ELLY.System.prototype.s = ELLY.System.prototype.scale;
