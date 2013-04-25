2702d25ec1641ac329be3ee96c40e3112a1a0b5d
Philipp Graf <PhilippGraf@web.de>
2013-04-25 12:50:34 +0200

    refactor init etc. to ELLY.Runner.js

--- public_html/js/ELLY.Runner.js
+++ public_html/js/ELLY.Runner.js
@@ -1,6 +1,84 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+// (C) Philipp Graf
+
+var ELLY = ELLY || {};
+
+ELLY.Runner = (function() {
+    var that;
+    
+    console.debug(that);
+    
+    var runner = function(parentElement) {
+        that = this;
+        
+        var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
+        var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 20000;
+        
+        // RENDERER
+        if (Detector.webgl) {
+            this.renderer = new THREE.WebGLRenderer({antialias: true});
+        } else {
+            this.renderer = new THREE.CanvasRenderer();
+        }
+        this.renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+        this.container = document.createElement('div');
+        parentElement.appendChild(this.container);
+        this.container.appendChild(this.renderer.domElement);
+        
+        // CAMERA
+        this.camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+        this.camera.position.set(-25, -10, 10);
+        this.camera.lookAt(0);
+ 
+        // EVENTS
+        THREEx.FullScreen.bindKey({charCode: 'f'.charCodeAt(0)});
+        THREEx.WindowResize(this.renderer, this.camera);
+
+        // CONTROLS
+        this.controls = new THREE.TrackballControls(this.camera);
 
 
+
+
+
+        
+    };
+
+    runner.prototype.run = function(script) {
+
+        // SCENE
+        this.scene = new THREE.Scene();
+        this.scene.add(this.camera);
+
+        // LIGHT
+        var light = new THREE.PointLight(0xffeeee, 1.3);
+        light.position.set(10, 10, 10);
+        this.scene.add(light);
+        var light2 = new THREE.PointLight(0xeeeeff, 1.0); // soft white light
+        light2.position.set(-10, -10, -10);
+        this.scene.add(light2);
+
+        // ELLY
+        this.elly = new ELLY.System(this.scene, script);
+        this.elly.trigger();
+
+        animate();
+
+    };
+
+     var animate = function() {
+        requestAnimationFrame(animate);
+        render();
+        update();
+    };
+
+    var render = function() {
+        that.renderer.render(that.scene, that.camera);
+    };
+    
+    var update = function() {
+        that.controls.update();
+    };
+    
+    return runner;
+})();
